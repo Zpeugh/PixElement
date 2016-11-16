cd193e78d45720b60375903d146a496f3936fd17
Zpeugh <zachpeugh@gmail.com>
2016-11-16 01:54:07 -0500

    First iteration.  Works on a given axis and arbitrary degrees of rotation.

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 #PixElement Code Challenge#
 
 ###Project specifics:
--If you're using any 3rd-party libraries, please include instructions for including/running them.
--When run, the software should open an .obj file. I will be using this one to test your code. (Picture)
--The code should, by default, rotate all the vertices of the 3D model 90 degrees along some axis that intersects the model.
--The program should then save a new, valid .obj file to disk with the rotated model.
+1. If you're using any 3rd-party libraries, please include instructions for including/running them.
+2. When run, the software should open an .obj file. I will be using this one to test your code. (Picture)
+3. The code should, by default, rotate all the vertices of the 3D model 90 degrees along some axis that intersects the model.
+4. The program should then save a new, valid .obj file to disk with the rotated model.
 
 ###Bonus points (entirely optional; pick-and-choose):
 | Bonus Task Description                                                                                                                                       | Status |
 |--------------------------------------------------------------------------------------------------------------------------------------------------------------|--------|
 | Use Python                                                                                                                                                   | Done   |
 | Use git, and commit frequently.                                                                                                                              |        |
-| Let me know how much time the project took. (No need to rush it.)                                                                                            |        |
+| Let me know how much time the project took. (No need to rush it.)                                                                                            |3 hours |
 | Use CloudCompare, Blender, or another tool to take a screenshot of the input and output.                                                                     |        |
 | Rotate around either the mean of the vertex coordinates, or around the center of the object's volume (the box containing the points) based on a boolean flag |        |
 | Let the user specify what input file to use via command-line argument.                                                                                       |        |
@@ -20,3 +20,10 @@
 | Let the user specify the axis of rotation as a vector.                                                                                                       |        |
 | Write automated tests for your code (or, even better, test-drive your code)                                                                                  |        |
 | Don't use any (explicit) loops. (a.k.a. map/reduce paradigm)                                                                                                 |        |
+
+
+
+####Running the program
+From a terminal, when in this directory use the command
+`python rotate.py -h`
+to get a list of all of the options for rotating a .obj file
--- rotate.py
+++ rotate.py
@@ -6,3 +6,121 @@
                   outputs a new object file rotated along
                   the center axis that number of degrees
 '''
+
+import argparse
+import numpy as np
+import math
+
+VERTEX_MARKER = 'v'
+OBJ_EXT = ".obj"
+
+
+'''
+    Given a line of three numbers seperated by spaces, return a
+    numpy array of three floats
+'''
+def read_three_floats(line):
+    line = line.strip()
+    return np.array([float(x) for x in line.split()[1:4]])
+
+'''
+    Given a file name, load in the .obj file returning
+    <numpy array of vertices>, <all other lines in the file>
+'''
+def load_object(file_name):
+
+    vertices = np.array([])
+    other_lines = []
+    with open(file_name) as f:
+        for line in f:
+            if line[0] == VERTEX_MARKER and line[1] == ' ':
+                if len(vertices) == 0:
+                    vertices = read_three_floats(line)
+                    print(vertices)
+                else:
+                    vertices = np.vstack((vertices, read_three_floats(line)))
+            else:
+                other_lines.append(line)
+    return vertices, other_lines
+
+
+'''
+    Returns a rotation matrix as a numpy array with shape (3,3)
+
+    axis    an arraylike object with length 3, indicating the axis
+            of rotation
+    deg     the number of degrees to rotate the object counterclockwise
+'''
+def rotation_matrix(axis, deg):
+    rad = math.radians(deg)
+    axis = np.array(axis)
+    axis_dot = np.dot(axis, axis)
+    if axis_dot == 0:
+        raise Exception("Invalid axis of rotation {0}".format(axis))
+    axis = axis/math.sqrt(axis_dot)
+    a = math.cos(rad/2.0)
+    b, c, d = -axis*math.sin(rad/2.0)
+    return np.array([[a*a+b*b-c*c-d*d, 2*(b*c+a*d), 2*(b*d-a*c)],
+                     [2*(b*c-a*d), a*a+c*c-b*b-d*d, 2*(c*d+a*b)],
+                     [2*(b*d+a*c), 2*(c*d-a*b), a*a+d*d-b*b-c*c]])
+
+
+'''
+    Given a 3D vertex point, write out the appropriate line for .obj files
+'''
+def create_vertex_line(vertex):
+    return "{0} {1} {2} {3}\n".format(VERTEX_MARKER, vertex[0], vertex[1], vertex[2])
+
+
+'''
+    Write an array of vertices and the accompanying other lines to the
+    specified file name in a .obj format
+'''
+def write_object(file_name, vertices, other_lines):
+
+    if (OBJ_EXT not in file_name):
+        file_name = file_name + OBJ_EXT
+
+    f = open(file_name, 'w')
+
+    for line in other_lines:
+        f.write(line)
+
+    f.write("\n")
+
+    for vertex in vertices:
+        f.write(create_vertex_line(vertex))
+
+    f.close()
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(prog='rotate', description='Rotate a .obj file object about an axis and write the output to disk')
+
+    # positional, mandatory arguments
+    parser.add_argument('input_file', help="The input .obj file name")
+    parser.add_argument('output_file', help="The output file name")
+
+    # optional arguments
+    parser.add_argument('-deg', help="The number of degrees to rotate the object counterclockwise by")
+    parser.add_argument('-ax', help="3D array of axis to rotate by.  i.e. [0,0,1]")
+
+    args = parser.parse_args()
+
+    if not args.deg:
+        args.deg = 90
+    if not args.ax:
+        args.ax = [0,0,1]
+    return args
+
+
+################################### Begin Script ###################################
+
+args = get_arguments()
+
+vertices, other_lines = load_object(args.input_file)
+
+rot_mat = rotation_matrix(args.ax, args.deg)
+vertices = np.dot(vertices, rot_mat)
+
+write_object(args.output_file, vertices, other_lines)
